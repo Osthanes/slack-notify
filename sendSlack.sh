45f8329b5f88ea28c6e6ffb90bd0d579e5425b1a
Robbie Minshall <rjminsha@us.ibm.com>
2015-06-11 14:02:33 -0400

    adding IDS sender address

--- sendSlack.sh
+++ sendSlack.sh
@@ -46,6 +46,23 @@ else
 fi 
 MSG=$(echo "$*" | sed 's/"/\\\"/g')
 
+# If we are running in an IDS job set a URL for the sender 
+if [ -n "${IDS_PROJECT_NAME}" ]; then 
+    echo "setting sender"
+    MY_IDS_PROJECT=${IDS_PROJECT_NAME##*| } 
+    MY_IDS_USER=${IDS_PROJECT_NAME%% |*}
+    if [ -z "${IDS_TARGET}" ]; then 
+        IDS_TARGET="https://hub.jazz.net"
+    fi 
+    IDS_URL="${IDS_TARGET}/pipeline/${MY_IDS_USER}/${MY_IDS_PROJECT}"
+    SENDER="<${IDS_URL}|${MY_IDS_PROJECT}-${MY_IDS_USER}>"
+    MSG="${SENDER}: ${MSG}"
+    echo ${MSG}
+else
+    echo "not setting sender:${IDS_PROJECT_NAME}"
+fi 
+
+echo $PAYLOAD
 PAYLOAD="{\"attachments\":[{""\"text\": \"$MSG\", \"color\": \"$SLACK_COLOR\"}]}"
 
 curl -X POST --data-urlencode "payload=$PAYLOAD" $URL
