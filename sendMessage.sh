2fa952deee1a3d762086798e6b34a6d2d77f61dc
Bahram Sanaei <sanaei@us.ibm.com>
2015-06-17 14:44:25 -0400

    added the NOTIFY_FILTER conditions and check for the retun code for the curl command

--- sendMessage.sh
+++ sendMessage.sh
@@ -49,7 +49,7 @@ Usage: [-m notify_message]
 Options:
   -m    Use notification massage for user input
   -l    Use notification level for user input. You can set the notification level using the NOTIFY_LEVEL environment variable.
-        Valid values are 'good', 'info', or 'bad'. 
+        Valid values are 'good', 'info', and 'bad'. 
   -h    Display this help message and exit
 
 Notes:
@@ -65,6 +65,24 @@ Notes:
     It is an optional environment variable.
     The value can either be one of 'good', 'warning', 'danger', or any hex color code (eg. #439FE0).
     If you set this optional environment, then, you don't need to set '-l notify_level' option when you call this script.
+  NOTIFY_FILTER: Specify the message filter level.
+    It is an optional environment variable.
+    The value can either be one of 'good', 'info', and 'bad'.
+    The table below show with 'X" wjen the notification message will be send based on setting notification level and NOTIFY_FILTER.
+    |---------------|--------------------------------------|
+    |               |             NOTIFY_FILTER            |
+    |---------------|---------|---------|--------|---------|
+    |  notification | unknown |   bad   |  good  |  info   |
+    |     level     |         |         |        |         |
+    |---------------|---------|---------|--------|---------|
+    |    unknown    |    X    |    X    |    X   |   X     |                     
+    |---------------|---------|---------|--------|---------|
+    |    bad        |    X    |    X    |    X   |   X     |
+    |---------------|---------|---------|--------|---------|
+    |    good       |    X    |         |    X   |   X     |
+    |---------------|---------|---------|--------|---------|
+    |    info       |         |         |        |   X     |
+    |---------------|---------|---------|--------|---------|
      
 EOF
 }
@@ -127,7 +145,7 @@ INVALID_ARGUMENTS=$*
 # If SLACK_COLOR is not set, then check the NOTIFY_LEVEL and set it to the SLACK_COLOR.
 # If both SLACK_COLOR and NOTIFY_LEVEL are not set, then don't specify the color by setting SLACK_COLOR to null. 
 if [ -z "$SLACK_COLOR" ]; then 
-    if [ -n "$NOTIFY_MSG" ] && [ -n "NOTIFY_LEVEL" ]; then
+    if [ -n "$NOTIFY_MSG" ] && [ -n "$NOTIFY_LEVEL" ]; then
         NOTIFY_LEVEL=$(echo $NOTIFY_LEVEL | tr '[:upper:]' '[:lower:]')
         case $NOTIFY_LEVEL in
             GOOD|good)
@@ -142,41 +160,95 @@ if [ -z "$SLACK_COLOR" ]; then
     fi
 else
     SLACK_COLOR=$(echo $SLACK_COLOR | tr '[:upper:]' '[:lower:]')
-fi 
-
-# Check if the message token has been set
-if [ -z "$SLACK_WEBHOOK_PATH" ]; then
-    die ${RC_SLACK_WEBHOOK_PATH}
+fi
+ 
+echo -e "Input Info:  SLACK_COLOR = '${SLACK_COLOR}', NOTIFY_FILTER = '${NOTIFY_FILTER}', NOTIFY_LEVEL = '${NOTIFY_LEVEL}', NOTIFY_MSG = '${NOTIFY_MSG}'"
+ 
+sendMsg=false 
+if [ -z "$NOTIFY_FILTER" ]; then 
+    if [ -z "$NOTIFY_LEVEL" ] || [ "$NOTIFY_LEVEL" == "bad" ] [ "$NOTIFY_LEVEL" == "good" ]; then
+        sendMsg=true
+    fi
+elif 
+    NOTIFY_FILTER=$(echo $NOTIFY_FILTER | tr '[:upper:]' '[:lower:]')
+    if [ "$NOTIFY_FILTER" == "bad" ]; then
+        if [ -z "$NOTIFY_LEVEL" ] || [ "$NOTIFY_LEVEL" == "bad" ]; then
+            sendMsg=true
+        fi
+    elif [ "$NOTIFY_FILTER" == "good" ]; then
+        if [ -z "$NOTIFY_LEVEL" ] || [ "$NOTIFY_LEVEL" == "bad" ] || [ "$NOTIFY_LEVEL" == "good" ]; then
+            sendMsg=true
+        fi
+    elif [ "$NOTIFY_FILTER" == "info" ]; then
+        if [ -z "$NOTIFY_LEVEL" ] || [ "$NOTIFY_LEVEL" == "bad" ] || [ "$NOTIFY_LEVEL" == "good" ] || [ "$NOTIFY_LEVEL" == "info" ]; then
+            sendMsg=true
+        fi    
+    else
+        if [ "$NOTIFY_LEVEL" == "info" ]; then
+            sendMsg=true
+        fi
+    fi
 fi
 
-# Send message to the Slack
-if [ -n "$SLACK_WEBHOOK_PATH" ]; then
-    echo $SLACK_WEBHOOK_PATH | grep "https://hooks.slack.com/services/"
-    FULL_PATH=$?
-    if [ $FULL_PATH -ne 0 ]; then 
-        URL="https://hooks.slack.com/services/$SLACK_WEBHOOK_PATH"
-    else 
-        URL=$SLACK_WEBHOOK_PATH
-    fi 
-
-    MSG="${NOTIFY_MSG}"
-
-    # If we are running in an IDS job set a URL for the sender 
-    if [ -n "${IDS_PROJECT_NAME}" ]; then 
-        echo -e "setting sender"
-        MY_IDS_PROJECT=${IDS_PROJECT_NAME##*| } 
-        MY_IDS_USER=${IDS_PROJECT_NAME%% |*}
-        MY_IDS_URL="${IDS_URL}/${MY_IDS_USER}/${MY_IDS_PROJECT}"
-        SENDER="<${MY_IDS_URL}|${MY_IDS_PROJECT}-${MY_IDS_USER}>"
-        MSG="${SENDER}: ${NOTIFY_MSG}"
+if [ "$sendMsg" == false ]; then
+    echo -e "Ignoring to send Notification message because the NOTIFY_FILTER = ${NOTIFY_FILTER} and NOTIFY_LEVEL = ${NOTIFY_LEVEL}"
+else
+    # Check if the message token has been set
+    if [ -z "$SLACK_WEBHOOK_PATH" ]; then
+        die ${RC_SLACK_WEBHOOK_PATH}
     else
-        echo "not setting sender:${IDS_PROJECT_NAME}"
-    fi 
+        echo -e "Slack Webhook URL token: '${SLACK_WEBHOOK_PATH}'"
+    fi
 
-    echo -e "Slack message: ${MSG}"
+    # Send message to the Slack
+    if [ -n "$SLACK_WEBHOOK_PATH" ]; then
+        echo $SLACK_WEBHOOK_PATH | grep "https://hooks.slack.com/services/"
+        FULL_PATH=$?
+        if [ $FULL_PATH -ne 0 ]; then 
+            URL="https://hooks.slack.com/services/$SLACK_WEBHOOK_PATH"
+        else 
+            URL=$SLACK_WEBHOOK_PATH
+        fi 
 
-    PAYLOAD="{\"attachments\":[{""\"text\": \"$MSG\", \"color\": \"$SLACK_COLOR\"}]}"
-    echo -e "Slack Payload: ${PAYLOAD}"
+        MSG="${NOTIFY_MSG}"
 
-    curl -X POST --data-urlencode "payload=$PAYLOAD" $URL
+        # If we are running in an IDS job set a URL for the sender 
+        if [ -n "${IDS_PROJECT_NAME}" ]; then 
+            echo -e "setting sender"
+            MY_IDS_PROJECT=${IDS_PROJECT_NAME##*| } 
+            MY_IDS_USER=${IDS_PROJECT_NAME%% |*}
+            MY_IDS_URL="${IDS_URL}/${MY_IDS_USER}/${MY_IDS_PROJECT}"
+            SENDER="<${MY_IDS_URL}|${MY_IDS_PROJECT}-${MY_IDS_USER}>"
+            MSG="${SENDER}: ${NOTIFY_MSG}"
+        else
+            echo -e "the notification sender is not defined"
+        fi 
+
+        echo -e "Sending notification message:  '${NOTIFY_MSG}'"
+
+        PAYLOAD="{\"attachments\":[{""\"text\": \"$MSG\", \"color\": \"$SLACK_COLOR\"}]}"
+        echo -e "Slack Payload: ${PAYLOAD}"
+
+        RESPONSE=$(curl --write-out %{http_code} --silent --output /dev/null -X POST --data-urlencode "payload=$PAYLOAD" $URL)
+        RESULT=$?
+        if [ "$RESULT" -eq 0 ]; then
+            if [ "$RESPONSE" -eq 200 ]; then
+                echo -e "${green}Slack notification message has been sent succesfully.${no_color}"
+                exit 0
+            elif [ "$RESPONSE" -eq 404 ]; then
+                echo -e "${red}Slack notification message has been failed with (Response code = ${RESPONSE} 'Bad Slack Webhook URL token'.${no_color})"
+                exit 1
+            elif [ "$RESPONSE" -eq 500 ]; then
+                echo -e "${red}Slack notification message has been failed with (Response code = ${RESPONSE} 'Salck Payload was not valid'.${no_color})"
+                exit 1
+            else
+                echo -e "${red}Slack notification message has been failed with (Response code = ${RESPONSE}).${no_color}"
+                exit 1
+            fi
+        else
+            echo -e "${red}curl command failed with retun code value ${RESULT}${no_color}"
+            exit 1
+
+        fi
+   fi
 fi
